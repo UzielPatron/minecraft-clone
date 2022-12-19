7142f62be5de75d88dd2180804ae0efda2fffc26
Uziel Patron <uzielpatron2020@gmail.com>
2022-12-18 21:33:36 -0300

    sky settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
 
 const App = () => {
   return (
     <Canvas>
-      <Sky />
+      <Sky sunPosition={[100, 100, 20]} />
+      <ambientLight intensity={0.5} />
+      <Physics />
     </Canvas>
   )
 }
