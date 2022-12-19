0cb9bf0b5c692bc5d9b65f8e915e834bee163d52
Uziel Patron <uzielpatron2020@gmail.com>
2022-12-18 21:27:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Canvas } from '@react-three/fiber'
+import { Sky } from '@react-three/drei'
+
+const App = () => {
+  return (
+    <Canvas>
+      <Sky />
+    </Canvas>
+  )
+}
+
+export default App
