cf8ae42a36e4388298a382873b8c3109677a0cc9
Uziel Patron <uzielpatron2020@gmail.com>
2022-12-18 21:44:39 -0300

    fpv fixed

--- src/components/FPV.jsx
+++ src/components/FPV.jsx
@@ -2,7 +2,7 @@ import { PointerLockControls } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 
 export const FPV = () => {
-  const [camera, gl] = useThree()
+  const { camera, gl } = useThree()
 
   return (
     <PointerLockControls args={[camera, gl.DomElement]} />
