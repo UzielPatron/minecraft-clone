b686f03715c2f99b1bbd5fae83302cba7ccbc863
Uziel Patron <uzielpatron2020@gmail.com>
2022-12-18 22:03:50 -0300

    textures added

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,4 +1,5 @@
 import { usePlane } from '@react-three/cannon'
+import { groundTexture } from '../images/textures'
 
 export const Ground = () => {
   const [ref] = usePlane(() => ({
@@ -6,10 +7,12 @@ export const Ground = () => {
     position: [0, -0.5, 0]
   }))
 
+  groundTexture.repeat.set(100, 100)
+
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach='geometry' args={[100, 100]} />
-      <meshStandardMaterial attach='material' color='green' />
+      <meshStandardMaterial attach='material' map={groundTexture} />
     </mesh>
   )
 }
---
+++ src/images/textures.js
@@ -0,0 +1,31 @@
+import { NearestFilter, RepeatWrapping, TextureLoader } from 'three'
+import {
+  grassImg,
+  dirtImg,
+  glassImg,
+  logImg,
+  woodImg
+} from './images'
+
+const groundTexture = new TextureLoader().load(grassImg)
+const dirtTexture = new TextureLoader().load(dirtImg)
+const glassTexture = new TextureLoader().load(glassImg)
+const logTexture = new TextureLoader().load(logImg)
+const woodTexture = new TextureLoader().load(woodImg)
+
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
+groundTexture.magFilter = NearestFilter
+
+dirtTexture.magFilter = NearestFilter
+glassTexture.magFilter = NearestFilter
+logTexture.magFilter = NearestFilter
+woodTexture.magFilter = NearestFilter
+
+export {
+  groundTexture,
+  dirtTexture,
+  glassTexture,
+  logTexture,
+  woodTexture
+}
